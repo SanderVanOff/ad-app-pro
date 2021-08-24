95ec9668c383529d34778c6133edfa8473aa793e
Sander <7okay@mail.ru>
2021-08-24 21:57:25 +0500

    - добавление компонента для фильтра объявлений;

---
+++ src/components/ui/appCategories.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="btn-group pb-3 d-flex" role="group" aria-label="Basic outlined example">
+    <button 
+    v-for="category of categories"
+    :key="category.id"
+    :ref="`category${category.id}`"
+    @click="$emit('changeCategory', category.id), getRefs(category.id)"
+    type="button" class="btn btn-outline-dark" style="font-size: 0.7rem!important;">{{category.title}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "appCategories",
+    props: {
+      categories: {
+        type: Array,
+      }
+    },
+    methods: {
+      getRefs(id){
+        let butRef = `category${id}`
+        Object.keys(this.$refs).forEach(key => {
+          this.$refs[key][0].classList.remove('active')
+        })
+        this.$refs[butRef][0].classList.add('active')
+      }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,27 @@
 <template>
   <v-main-section>
+  <app-categories 
+    @changeCategory="changeCategory"
+    :categories=categories></app-categories>
     <!-- Loading -->
     <Loader v-if="loading" />
     <!-- empty -->
-    <v-no-ad v-else-if="!ads.length"/>
+    <v-no-ad v-else-if="!filteredAd.length"/>
 
     <!-- ads -->
-    <transition-group name="list" tag="div" class="cards" v-else>
+    <div class="home" v-else>
+    
+
+    <transition-group name="list" mode="out-in" tag="div" class="cards" >
       <v-card
-        v-for="card of activeAds"
+        v-for="card of filteredAd"
         :key="card.id"
         :cardItem="card"
         @go-to-card-info="goToCardInfo(card.id)"
         @favorite-ad="likesAd(card.id)"
       ></v-card>
     </transition-group>
+    </div>
   </v-main-section>
 </template>
 
@@ -23,6 +30,7 @@
 import vMainSection from "@/components/ui/vMainSection.vue";
 import vCard from "@/components/ui/vCard.vue";
 import vNoAd from "@/components/ui/vNoAd.vue";
+import appCategories from "@/components/ui/appCategories.vue";
 //vuex
 import { mapActions, mapGetters } from "vuex";
 export default {
@@ -31,6 +39,7 @@ export default {
     vMainSection,
     vCard,
     vNoAd,
+    appCategories
   },
   computed: {
     ...mapGetters(["allAds", "currentUID"]),
@@ -43,11 +52,14 @@ export default {
     loading: false,
     counter: 0,
     ads: [],
+    filteredAd: [],
+    categories: []
   }),
   methods: {
     ...mapActions([
       "fetchAdsFromDB",
       "addFavoriteAdToUser",
+      "fetchCategories"
     ]),
     async goToCardInfo(id) {
       
@@ -59,10 +71,16 @@ export default {
     async likesAd(id) {
       await this.addFavoriteAdToUser(id);
     },
+
+    changeCategory(id){
+      this.filteredAd = this.ads.filter(ad => ad.categoryId === id)
+    } 
   },
   async mounted() {
     this.loading = true;
+    this.categories = await this.fetchCategories()
     this.ads = await this.fetchAdsFromDB();
+    this.filteredAd = this.ads;
     this.loading = false;
   },
 };
